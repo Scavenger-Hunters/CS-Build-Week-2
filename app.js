e9bb27905a1f663218ad1f9727f6a98ce12b4734
Jorlost19 <jorlost19@gmail.com>
2019-08-27 02:23:38 -0500

    playing around with the graph

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const adv = require('./axiosConfig');
 
+currentRoom = {};
+
 adv
-  .get('init')
-  .then(res => console.log(res.data))
+  .post('move', { direction: 'n' })
+  .then(res => {
+    console.log(res.data);
+    currentRoom = res.data;
+  })
   .catch(err => console.error(err));
+// setTimeout(() => {
+//   adv
+//     .post('move', {
+//       direction: 'w'
+//     })
+//     .then(res => {
+//       console.log(res.data);
+//       currentRoom = res.data;
+//     });
+// }, currentRoom.cooldown * 1000);
