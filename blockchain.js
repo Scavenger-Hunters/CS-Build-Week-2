36b3cc7c729e75dc61945b08c29ecec07b8460d7
Sem8 <mssemmi8@gmail.com>
2019-08-27 21:44:49 -0400

    added crypto-js library to bring in SHA256 hash function and created block and blockchain classes with methods for calculating hash and mining blocks

---
+++ blockchain.js
@@ -0,0 +1,92 @@
+const adv = require("./axiosConfig");
+const SHA256 = require("crypto-js/sha256");
+
+// Initialization
+adv
+  .get("init")
+  .then(res => {
+    console.log("Init: ", res.data);
+
+    // Set Current Room as res.data
+    currentRoom = res.data;
+
+    // Check ID and Exit data
+    console.log("ID: ", currentRoom.room_id);
+    console.log("Exits: ", currentRoom.exits);
+
+    // get cooldown value for current room for setTimeout()
+    roomCD = currentRoom.cooldown;
+  })
+  .catch(err => console.error(err));
+
+// Creating Blockchain class
+
+class Block {
+  constructor(index, timestamp, data, previousHash = "") {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+    this.nonce = 0;
+  }
+
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data) +
+        this.nonce
+    ).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("Block mined: " + this.hash);
+  }
+}
+
+class BlockChain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 2;
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "01/01/2017", "Genesis block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    // newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
